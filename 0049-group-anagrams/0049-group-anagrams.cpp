e0c12ba61844257be28dc9d3440e71923441c050
Rizul Thakur <81066183+0xrizy@users.noreply.github.com>
2023-02-13 22:23:44 +0530

    Time: 31 ms (92.25%), Space: 19.7 MB (74.45%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> map;
+        vector<vector<string>> ans;
+        for(int i=0;i<strs.size();i++){
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            if(map.count(temp)){
+                map[temp].push_back(strs[i]);
+            }
+            else{
+                map[temp].push_back(strs[i]);
+            }
+        }
+        
+        for(auto i = map.begin(); i != map.end(); i++)
+            ans.push_back(i -> second);
+        return ans;
+        
+    }
+};
